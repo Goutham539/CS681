fd2a54160624983098cdfae45a7a8af69e17d1bd
Goutham Nerella <45123323+Goutham539@users.noreply.github.com>
2022-05-20 17:14:49 -0400

    HW11

--- HW11/src/edu/umb/cs681/hw11/AccessCounter.java
+++ HW11/src/edu/umb/cs681/hw11/AccessCounter.java
@@ -7,31 +7,31 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class AccessCounter {
 	
-	private Map<java.nio.file.Path, Integer> mapHashMap = new HashMap<java.nio.file.Path, Integer>();
+	private Map<java.nio.file.Path, Integer> Hm = new HashMap<java.nio.file.Path, Integer>();
 	private ReentrantLock rLock = new ReentrantLock();
-	private static ReentrantLock staticLock = new ReentrantLock();
+	private static ReentrantLock sL = new ReentrantLock();
 	private static AccessCounter instance = null;
 	private AccessCounter() {};	
 	
 	public static AccessCounter getInstance() {
-		staticLock.lock();
+		sL.lock();
 		try {
 			if (instance == null)
 				instance = new AccessCounter();
 			return instance;
 		}
 		finally {
-			staticLock.unlock();
+			sL.unlock();
 		}
 	}
 	
 	public void increment(Path path) {
 		rLock.lock();
 		try {
-			if (mapHashMap.get(path) != null)
-				mapHashMap.put(path, mapHashMap.get(path) + 1);
+			if (Hm.get(path) != null)
+				Hm.put(path, Hm.get(path) + 1);
 			else 
-				mapHashMap.put(path, 1);
+				Hm.put(path, 1);
 		}
 		finally {
 			rLock.unlock();
@@ -41,13 +41,17 @@ public class AccessCounter {
 	public int getCount(Path path) {
 		rLock.lock();
 		try {
-			if (mapHashMap.get(path) != null)
-				return mapHashMap.get(path);
+			
+			if (Hm.get(path) != null)
+				return Hm.get(path);
 			else
 				return 0;
+			
 		}
 		finally {
+			
 			rLock.unlock();
+		
 		}
 	}	
 }
